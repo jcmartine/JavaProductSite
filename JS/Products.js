b025e9c9e2df20c6ac786703e074f0d3fd8e9b6f
Juan C Martinez <jcmartine@gmail.com>
2025-12-27 21:26:07 -0500

    first commit

---
+++ JS/Products.js
@@ -0,0 +1,45 @@
+const products = [
+  {
+    id: "Indian Food pack",
+    name: "Indian Food Pack",
+    category: "Food",
+    price: 29.99,
+    rating: 4.5,
+    reviews: 1200,
+    stock: 24,
+    badge: "Bestseller",
+    description: "Advanced ergonomic mouse with ultra-fast scrolling and silent clicks."
+  },
+  {
+    id: "Mexican Food pack",
+    name: "Mexican Food Pack",
+    category: "Food",
+    price: 24.99,
+    rating: 4.2,
+    reviews: 800,
+    stock: 15,
+    badge: "New",
+    description: "Authentic Mexican cuisine ingredients for a delicious meal."
+  },
+  {    id: "Italian Food pack",
+    name: "Italian Food Pack",
+    category: "Food",
+    price: 29.99,
+    rating: 4.5,
+    reviews: 1200,
+    stock: 24,
+    badge: "Bestseller",
+    description: "Authentic Italian cuisine ingredients for a delicious meal."
+  },
+  {    id: "Chinese Food pack",
+    name: "Chinese Food Pack",
+    category: "Food",
+    price: 29.99,
+    rating: 4.5,
+    reviews: 1200,
+    stock: 24,
+    badge: "Bestseller",
+    description: "Authentic Chinese cuisine ingredients for a delicious meal."
+  }
+];
+
